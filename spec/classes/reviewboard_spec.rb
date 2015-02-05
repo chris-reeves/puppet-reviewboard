0ee4e901dfcb52c33ed3c0c9ad9e98d679a5f888
Chris Reeves <chris.reeves@york.ac.uk>
2015-02-05 17:23:47 +0000

    Add tests for pkg_memcached parameter
    These test the default value, a custom package specified as a string, two
    custom packages passed as an array, and the special value 'NONE'.

--- spec/classes/reviewboard_spec.rb
+++ spec/classes/reviewboard_spec.rb
@@ -42,4 +42,27 @@ describe 'reviewboard' do
 
     it { should contain_exec('install reviewboard').with_command("easy_install '#{custom_egg_url}'") }
   end
+
+  context 'with pkg_memcached as default' do
+    it { should contain_package('memcached') }
+  end
+
+  context 'with pkg_memcached set to my-memcached' do
+    let (:params) { { :pkg_memcached => 'my-memcached' } }
+
+    it { should contain_package('my-memcached') }
+  end
+
+  context 'with pkg_memcached set to an array' do
+    let (:params) { { :pkg_memcached => [ 'my-memcached', 'my-python-memcached' ] } }
+
+    it { should contain_package('my-memcached') }
+    it { should contain_package('my-python-memcached') }
+  end
+
+  context 'with pkg_memcached set to NONE' do
+    let (:params) { { :pkg_memcached => 'NONE' } }
+
+    it { should_not contain_package('memcached') }
+  end
 end
