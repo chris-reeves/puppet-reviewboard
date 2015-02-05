518d253a2c4c1e7fa36641d756d1cf84da0b56f3
Chris Reeves <chris.reeves@york.ac.uk>
2015-02-05 17:31:00 +0000

    Group spec tests for main reviewboard class

--- spec/classes/reviewboard_spec.rb
+++ spec/classes/reviewboard_spec.rb
@@ -9,60 +9,63 @@ describe 'reviewboard' do
     it { should contain_class('reviewboard::install') }
   end
 
-  context 'with version set to 1.7.28 and no egg_url' do
-    let (:params) { { :version => '1.7.28' } }
+  context 'with version and egg_url' do
+    custom_egg_url = 'http://egg.example.com/ReviewBoard.egg'
 
-    it { should contain_exec('install reviewboard') }
+    context 'set to 1.7.28 and not specified, respectively' do
+      let (:params) { { :version => '1.7.28' } }
+
+      it { should contain_exec('install reviewboard') }
+    end
+
+    context 'set to 2.0.12 and not specified, respectively' do
+      let (:params) { { :version => '2.0.12' } }
+
+      it { should contain_exec('install reviewboard') }
+    end
+
+    context 'set to 1.8.0 and not specified, respectively' do
+      let (:params) { { :version => '1.8.0' } }
+
+      it { should raise_error(Puppet::Error, /Reviewboard module has not been tested with Reviewboard/) }
+    end
+
+    context "set to 1.7.28 and #{custom_egg_url}" do
+      let (:params) { { :version => '1.7.28', :egg_url => custom_egg_url } }
+
+      it { should contain_exec('install reviewboard').with_command("easy_install '#{custom_egg_url}'") }
+    end
+
+    # Shouldn't care about version number when egg_url is specified
+    context "set to 1.8.0 and #{custom_egg_url}" do
+      let (:params) { { :version => '1.8.0', :egg_url => custom_egg_url } }
+
+      it { should contain_exec('install reviewboard').with_command("easy_install '#{custom_egg_url}'") }
+    end
   end
 
-  context 'with version set to 2.0.12 and no egg_url' do
-    let (:params) { { :version => '2.0.12' } }
+  context 'with pkg_memcached' do
+    context 'left at the default' do
+      it { should contain_package('memcached') }
+    end
 
-    it { should contain_exec('install reviewboard') }
-  end
+    context 'set to my-memcached' do
+      let (:params) { { :pkg_memcached => 'my-memcached' } }
 
-  context 'with version set to 1.8.0 and no egg_url' do
-    let (:params) { { :version => '1.8.0' } }
+      it { should contain_package('my-memcached') }
+    end
 
-    it { should raise_error(Puppet::Error, /Reviewboard module has not been tested with Reviewboard/) }
-  end
+    context 'set to an array' do
+      let (:params) { { :pkg_memcached => [ 'my-memcached', 'my-python-memcached' ] } }
 
+      it { should contain_package('my-memcached') }
+      it { should contain_package('my-python-memcached') }
+    end
 
-  custom_egg_url = 'http://egg.example.com/ReviewBoard.egg'
+    context 'set to NONE' do
+      let (:params) { { :pkg_memcached => 'NONE' } }
 
-  context "with version set to 1.7.28 and egg_url set to #{custom_egg_url}" do
-    let (:params) { { :version => '1.7.28', :egg_url => custom_egg_url } }
-
-    it { should contain_exec('install reviewboard').with_command("easy_install '#{custom_egg_url}'") }
-  end
-
-  # Shouldn't care about version number when egg_url is specified
-  context "with version set to 1.8.0 and egg_url set to #{custom_egg_url}" do
-    let (:params) { { :version => '1.8.0', :egg_url => custom_egg_url } }
-
-    it { should contain_exec('install reviewboard').with_command("easy_install '#{custom_egg_url}'") }
-  end
-
-  context 'with pkg_memcached as default' do
-    it { should contain_package('memcached') }
-  end
-
-  context 'with pkg_memcached set to my-memcached' do
-    let (:params) { { :pkg_memcached => 'my-memcached' } }
-
-    it { should contain_package('my-memcached') }
-  end
-
-  context 'with pkg_memcached set to an array' do
-    let (:params) { { :pkg_memcached => [ 'my-memcached', 'my-python-memcached' ] } }
-
-    it { should contain_package('my-memcached') }
-    it { should contain_package('my-python-memcached') }
-  end
-
-  context 'with pkg_memcached set to NONE' do
-    let (:params) { { :pkg_memcached => 'NONE' } }
-
-    it { should_not contain_package('memcached') }
+      it { should_not contain_package('memcached') }
+    end
   end
 end
